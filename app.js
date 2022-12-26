6904b68dec1ba83b14d2e83052ab73f2442976b2
Yena-Yun <cheryleduart@gmail.com>
2022-12-26 13:23:54 +0900

    feat: 멈춤 누르면 canvas 완전 초기화, 일시정지 때 복제 누르면 화면에 시각적으로 반영, 버튼 활성화/비활성화 함수

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('mycanvas');
+const canvas = document.getElementById('canvas');
 
 const width = canvas.width;
 const height = canvas.height;
@@ -9,8 +9,8 @@ class Ball {
   constructor(x, y, speed, radius, color) {
     this.x = x;
     this.y = y;
-    this.velx = speed;
-    this.vely = speed;
+    this.velX = speed;
+    this.velY = speed;
     this.radius = radius;
     this.color = color;
   }
@@ -23,41 +23,39 @@ class Ball {
   }
 
   bounce() {
-    // 속도에 -1을 곱함: canvas 벽에 부딪히면 공이 반대방향으로 이동
     if (this.x + this.radius >= width || this.x - this.radius <= 0) {
-      this.velx *= -1;
+      this.velX *= -1; // 벽에 부딪히면 반대방향으로 이동
     }
 
     if (this.y + this.radius >= height || this.y - this.radius <= 0) {
-      this.vely *= -1;
+      this.velY *= -1;
     }
 
-    // 좌표 값에 속도를 더해서 공이 움직이게 함
-    this.x += this.velx;
-    this.y += this.vely;
+    // 좌표에 속도를 붙여 공이 움직이게 함
+    this.x += this.velX;
+    this.y += this.velY;
   }
 }
 
-// 공통 랜덤 함수
+// 랜덤 함수 (공의 움직임과 색깔을 랜덤으로 조정)
 function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 let powerCnt = 0; // 거듭제곱할 횟수
-let ballCnt = 1; // (거듭제곱으로 생성되는) 공의 개수
+let poweredBallCnt = 1; // (거듭제곱으로 생성되는) 공의 개수
 
-const balls = [];
+let balls = [];
 
 let x = 10;
 let y = 10;
 let radius = 5;
 
 function createBall() {
-  while (balls.length < ballCnt) {
+  while (balls.length < poweredBallCnt) {
     // ballCnt가 거듭제곱된 상태이면
-    if (ballCnt !== 1) {
-      // 공의 생성지점인 x, y 좌표에 랜덤값 부여
+    if (poweredBallCnt > 1) {
+      // 공이 생성되는 좌표값을 랜덤하게 만듦
       x = random(radius, width - radius);
       y = random(radius, height - radius);
     }
@@ -67,7 +65,7 @@ function createBall() {
       y,
       5,
       radius,
-      `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})` // 공에 랜덤 색깔 부여
+      `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})` // 공에 랜덤색 부여
     );
 
     balls.push(ball);
@@ -75,48 +73,62 @@ function createBall() {
 }
 
 let isPaused = false;
-let myReq; // cancelAnimationFrame의 기준 (이후 requestAnimationFrame을 할당)
+let myReq; // cancelAnimation의 기준
 
 function animate() {
-  // 다음 애니메이션 그리기 전에 이전 캔버스 초기화
+  // (다음 애니메이션을 그리기 전) 이전 캔버스 초기화
   ctx.fillStyle = '#fff';
   ctx.fillRect(0, 0, width, height);
 
-  // balls 배열을 돌면서 각각의 공에 그리기 함수와 튕기기 함수 실행
-  for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].bounce();
-  }
+  balls.map((ball) => {
+    ball.draw();
+    ball.bounce();
+  });
 
-  // isPaused가 false일 때만 애니메이션 실행
+  // 일시정지 상태가 아닐 때 애니메이션 실행
   if (!isPaused) {
     myReq = requestAnimationFrame(animate);
   }
 }
 
-// 시작 버튼
-const startBtn = document.getElementById('start');
-startBtn.addEventListener('click', () => {
-  createBall();
+// 버튼 활성화
+function activeButton(button) {
+  button.style.color = '#000';
+  button.style.cursor = 'pointer';
+  button.style.backgroundColor = '#eee';
+  button.style.pointerEvents = 'auto';
+}
 
-  // 시작버튼 비활성화
-  startBtn.style.color = '#c5c5c5';
-  startBtn.style.cursor = 'default';
-  startBtn.style.backgroundColor = '#eee';
-  startBtn.style.pointerEvents = 'none';
+// 버튼 비활성화
+function inactiveButton(button) {
+  button.style.color = '#c5c5c5';
+  button.style.cursor = 'default';
+  button.style.backgroundColor = '#eee';
+  button.style.pointerEvents = 'none';
+}
+
+// 시작 버튼
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', () => {
+  createBall();
+  inactiveButton(startButton);
+
+  activeButton(pauseButton);
+  activeButton(copyButton);
+  activeButton(stopButton);
 
   myReq = requestAnimationFrame(animate);
 });
 
-// 일시정지 버튼
-const pauseBtn = document.getElementById('pause');
-pauseBtn.addEventListener('click', () => {
+// 일시정지/다시재생 버튼
+const pauseButton = document.getElementById('pause');
+pauseButton.addEventListener('click', () => {
   isPaused = !isPaused;
 
   if (isPaused) {
-    pauseBtn.innerText = '다시재생';
+    pauseButton.innerText = '다시재생';
   } else {
-    pauseBtn.innerText = '일시정지';
+    pauseButton.innerText = '일시정지';
   }
 
   if (!isPaused) {
@@ -125,27 +137,46 @@ pauseBtn.addEventListener('click', () => {
 });
 
 // 복제 버튼
-const copyBtn = document.getElementById('copy');
-copyBtn.addEventListener('click', () => {
-  powerCnt++; // 거듭제곱 횟수: 버튼 누를 때마다 1씩 증가
-  ballCnt = Math.pow(4, powerCnt); // 거듭제곱으로 생성된 공의 개수
+const copyButton = document.getElementById('copy');
+copyButton.addEventListener('click', () => {
+  powerCnt++;
+  poweredBallCnt = Math.pow(4, powerCnt);
 
-  // ballCnt 개수만큼 공 생성
   createBall();
+
+  if (isPaused) {
+    balls.map((ball) => {
+      ball.draw();
+    });
+
+    myReq = requestAnimationFrame(animate);
+  }
 });
 
 // 멈춤 버튼
-const stopBtn = document.getElementById('stop');
-stopBtn.addEventListener('click', () => {
-  // 화면에 움직임이 있을 때 캔버스 초기화 (3D 애니메이션 삭제)
+const stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => {
+  inactiveButton(stopButton);
+  inactiveButton(pauseButton);
+  inactiveButton(copyButton);
+
+  // 모두 초기화
+  balls = [];
+  powerCnt = 0;
+  poweredBallCnt = 1;
+  isPaused = false;
+
+  // 3D 애니메이션 삭제
   cancelAnimationFrame(myReq);
 
-  // 화면에 움직임이 없을 때 캔버스 초기화 (2D 삭제)
+  // 2D 화면 삭제
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-  // 시작버튼 다시 활성화
-  startBtn.style.color = '#000';
-  startBtn.style.cursor = 'pointer';
-  startBtn.style.backgroundColor = '#eee';
-  startBtn.style.pointerEvents = 'auto';
+  activeButton(startButton);
 });
+
+window.onload = () => {
+  inactiveButton(pauseButton);
+  inactiveButton(copyButton);
+  inactiveButton(stopButton);
+};
